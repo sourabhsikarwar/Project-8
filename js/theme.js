9358e3c1561d238d21c45d05e47fbf80ab751ea0
Sourabh <sourabhsikarwar1812@gmail.com>
2021-09-08 12:14:39 +0530

    first commit

---
+++ js/theme.js
@@ -0,0 +1,10 @@
+
+// sidebar js
+
+const openside = () => {
+    document.getElementById('opensidebar').style.right="0";
+}
+const closesidebar = () => {
+    document.getElementById('opensidebar').style.right="-280px";
+}
+
